83eeda9aac464860775c8118c493c052239f0b14
Birattam Rajak <birattam444@gmail.com>
2023-04-17 19:52:37 +0530

    import all files to css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import Header from './MyComponets/Header';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
-  
+  Link
 } from "react-router-dom";
+
+import "./styles/App.scss"
+import "./styles/colors.scss"
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route />
       </Routes>
