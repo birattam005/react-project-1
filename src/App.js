192a9c17646a1b77073b446094e2d7528b53af80
Birattam Rajak <birattam444@gmail.com>
2023-04-17 16:37:08 +0530

    adding routers

--- src/App.js
+++ src/App.js
@@ -1,2 +1,20 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  
+} from "react-router-dom";
 
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
 
