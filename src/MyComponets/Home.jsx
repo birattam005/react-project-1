c12c64e4837ba227bcca6e7fd6358746ac9cde56
Birattam Rajak <birattam444@gmail.com>
2023-04-18 13:55:27 +0530

    home second create and style

--- src/MyComponets/Home.jsx
+++ src/MyComponets/Home.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import vg from "../assests/2.webp"
 
 const Home = () => {
   return (
+    <>
     <div className="home" id="home">
         <main>
           <h1>TechyStar</h1>
           <p>Solution to all your problems</p>
         </main>
       </div>
+
+    <div className="home2">
+        <img src={vg} alt="Graphics" />
+        <div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quo minima sapiente! Delectus aliquid, enim error ducimus mollitia deserunt sint.
+          </p>
+        </div>
+    </div>
+    </>
   )
 }
 
