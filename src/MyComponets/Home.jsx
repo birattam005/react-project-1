38aad61bbe57656f2ab73a72e1ee8ff70de09e41
Birattam Rajak <birattam444@gmail.com>
2023-04-18 14:29:06 +0530

    home div 4 complete

--- src/MyComponets/Home.jsx
+++ src/MyComponets/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import vg from "../assests/2.webp"
+import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram  } from "react-icons/ai";
 
 const Home = () => {
   return (
@@ -25,6 +26,42 @@ const Home = () => {
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur obcaecati distinctio in id, corrupti architecto illo illum ad necessitatibus sed amet sunt harum eum repellendus quasi earum reprehenderit similique impedit? Nulla culpa minus esse incidunt ducimus hic maiores voluptatibus, dolorem reiciendis tempora libero sunt impedit sequi unde cumque earum praesentium!</p>
         </div>
     </div>
+
+    <div className="home4">
+        <div>
+          <h1>Brands</h1>
+          <article>
+              <div style={{
+                animationDelay: "0.3s"
+              }}>
+                  <AiFillGoogleCircle />
+                  <p>Google</p>
+              </div>
+
+              <div style={{
+                animationDelay: "0.5s"
+              }}>
+                  <AiFillAmazonCircle />
+                  <p>Amazon</p>
+              </div>
+
+              <div style={{
+                animationDelay: "0.7s"
+              }}>
+                  <AiFillYoutube />
+                  <p>Youtube</p>
+              </div>
+
+              <div style={{
+                animationDelay: "0.1s"
+              }}>
+                  <AiFillInstagram />
+                  <p>Instagram</p>
+              </div>
+          </article>
+        </div>
+    </div>
+
     </>
   )
 }
