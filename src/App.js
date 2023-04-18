19ea171d24a36903b1b4ff035a080f1105a558fb
Birattam Rajak <birattam444@gmail.com>
2023-04-18 15:00:58 +0530

    footer complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './MyComponets/Header';
 import Home from './MyComponets/Home';
+import Footer from './MyComponets/Footer';
+
 import {
   BrowserRouter as Router,
   Route,
@@ -12,6 +14,7 @@ import "./styles/App.scss"
 import "./styles/colors.scss"
 import "./styles/Header.scss"
 import "./styles/home.scss"
+import "./styles/footer.scss"
 
 
 const App = () => {
@@ -21,6 +24,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />}/>
       </Routes>
+      <Footer />
     </Router>
   )
 }
---
+++ src/MyComponets/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+        <div>
+            <h1>Rising Star</h1>
+            <p>@all right reserved</p>
+        </div>
+        <div>
+            <h5>Follow us</h5>
+            <div>
+                <a href="https://github.com/birattam005" target={"blank"}>Github</a>
+                <a href="https://www.linkedin.com/in/birattam005/" target={"blank"}>Linkdin</a>
+                <a href="https://twitter.com/Biru32624195" target={"blank"}>Twiter</a>
+            </div>
+        </div>
+  </footer>
+}
+
+export default Footer
